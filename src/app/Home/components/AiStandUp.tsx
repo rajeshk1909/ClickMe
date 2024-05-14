5fb360217140fd0779613001e756ce6f20ab6a4f
rajeshk1909 <[rajeshkumark1909@gmail.com]>
2024-05-15 00:02:55 +0530

    Home Page design complite

--- src/app/Home/components/AiStandUp.tsx
+++ src/app/Home/components/AiStandUp.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineFullscreen } from "react-icons/ai"
 import { MdOutlineMoreHoriz } from "react-icons/md"
+import { PiStarFourFill } from "react-icons/pi"
 
 const AiStandUp = () => {
   return (
@@ -8,9 +9,7 @@ const AiStandUp = () => {
         <div className='flex w-[97%] justify-between items-center font-semibold '>
           <h1 className='ml-2'>Line Up</h1>
           <div className='flex items-center '>
-            <button className='hover:cursor-not-allowed flex items-center  rounded-md'>
-              <AiOutlineFullscreen className=' hover:cursor-not-allowed text-gray-400 m-1 cursor-pointer text-base' />
-            </button>
+              <AiOutlineFullscreen className=' cursor-not-allowed text-gray-400 m-1 text-base' />
             <button className='hover:bg-gray-200 flex items-center  rounded-md'>
               <MdOutlineMoreHoriz className=' m-1 cursor-pointer text-base' />
             </button>
@@ -19,10 +18,11 @@ const AiStandUp = () => {
 
         <div className='flex items-center h-[100%] justify-center'>
           <div className='flex items-center flex-col'>
+            <PiStarFourFill className='text-[30px] text-[#ff37af]' />
             <p className='text-xs mt-5'>
               Use ClickUp AI to create a recurring summary of
             </p>
-            <p  className='text-xs'>recent activity.</p>
+            <p className='text-xs'>recent activity.</p>
             <div className='mt-5'>
               <button className=' bg-[#7b68ee]  hover:bg-[#5f48ea] text-xs font-semibold text-white rounded-md py-1 px-5'>
                 {" "}
